118d10019f666fa47ae5b405d81021567c341ef1
Philip Gyamfi <philab.pg@gmail.com>
2023-09-12 07:10:43 +0000

    initial project and db setup

---
+++ db/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource, DataSourceOptions } from "typeorm";
+import {config} from "dotenv";
+
+config();
+
+export const dataSourceOptions: DataSourceOptions = {
+    type: "postgres",
+    host: `${process.env.HOST}`,
+    port: Number.parseInt(`${process.env.POSTGRES_DB_PORT}`),
+    username: `${process.env.POSTGRES_USER}`,
+    password: `${process.env.POSTGRES_PASSWORD}`,
+    database: `${process.env.POSTGRES_DB}`,
+    entities: ['dist/**/*.entity.js'],
+    migrations: ['dist/db/migrations/*.js'],
+}
+
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from 'db/data-source';
+
+@Module({
+  imports: [ConfigModule.forRoot({
+    envFilePath: ".env"
+  }), TypeOrmModule.forRoot(dataSourceOptions)],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
